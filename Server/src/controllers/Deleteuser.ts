509b76709ffce17fe72689f112f9b67bf4cde9db
M-Tijani <150944603+M-Tijani@users.noreply.github.com>
2024-04-22 11:56:30 -1200

    making New Stuff

--- Server/src/controllers/Deleteuser.ts
+++ Server/src/controllers/Deleteuser.ts
@@ -3,12 +3,12 @@ import { Request, Response, NextFunction } from "express";
 import { User } from "../models/user_schema";
 const Deleteuser = async (req: Request, res: Response, next: NextFunction) => {
   // Delete User
-  const _id = req.cookies.Authorization;
+  const _id: string = req.cookies.Authorization;
   try {
     const user = await User.findByIdAndDelete(_id);
-    res.status(200).send({ message: "User deleted successfully", user });
   } catch (error) {
-    res.status(500).send({ message: error });
+    console.log(error);
+    // res.status(500).send({ message: error });
   }
 };
 
